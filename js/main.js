7bfe5c750dc4a30b3b0e95c4ba135fb719e82b79
Mostafa Esmail <100853809+e5m41l@users.noreply.github.com>
2024-12-09 01:59:59 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+function showAdd() {
+
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "2000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+    Command: toastr["success"]("User was added :)", "Success!");
+}
+
+
+function confirmDelete(id) {
+  let del = confirm("Do you really want to delete this user?");
+  // console.log(del);
+  // console.log(id);
+
+  if (del) {
+    window.location.href="index.php?action=del&&id="+id;
+  }
+}
+
+function editUser(id) {
+  // console.log(id);
+  window.location.href="add_users.php?action=edit&&id="+id;
+}
+
+function showDeleted() {
+
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "2000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+    Command: toastr["Warning"]("User was Deleted", "Deleted!");
+}
+
+function showUpdated() {
+
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "2000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+    Command: toastr["Warning"]("User updated", "Updated!");
+}
